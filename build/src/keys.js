8773c8c0d3de9b6efe5c4955037e9089342adf23
grim <fedeagrimaldi@gmail.com>
2022-10-14 18:48:30 -0300

    14-10-2022

--- build/src/keys.js
+++ build/src/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'grim',
+        host: 'us-cdbr-east-06.cleardb.net',
+        user: 'be6c8ab1e82e28',
+        password: 'c8888b73',
+        database: 'heroku_b8b09a5ff9918e5',
     }
 };
