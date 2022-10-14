119ff7959fae98577690844f5a45e237706b7401
grim <fedeagrimaldi@gmail.com>
2022-10-13 23:31:38 -0300

    keys

--- src/keys.ts
+++ src/keys.ts
@@ -1,8 +1,8 @@
 export default{
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'grim',
+        host: 'us-cdbr-east-06.cleardb.net',
+        user: 'be6c8ab1e82e28',
+        password: 'c8888b73',
+        database: 'heroku_b8b09a5ff9918e5',
     }
 }
